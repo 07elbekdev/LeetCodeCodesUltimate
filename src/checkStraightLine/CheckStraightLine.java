aa056b6e506dc755e3982c34a8fc87459295acbc
07elbekdev <masrapovelbek24@gmail.com>
2023-01-14 18:55:30 +0600

    I committed something.

--- src/checkStraightLine/CheckStraightLine.java
+++ src/checkStraightLine/CheckStraightLine.java
@@ -2,6 +2,19 @@ package checkStraightLine;
 
 public class CheckStraightLine {
     public static void main(String[] args) {
+        int[][] array = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+        System.out.println(checkStraightLine(array));
+    }
 
+    public static boolean checkStraightLine(int[][] coordinates) {
+        int x0 = coordinates[0][0], y0 = coordinates[0][1],
+                x1 = coordinates[1][0], y1 = coordinates[1][1];
+        int dx = x1 - x0, dy = y1 - y0;
+        for (int[] co : coordinates) {
+            int x = co[0], y = co[1];
+            if (dx * (y - y1) != dy * (x - x1))
+                return false;
+        }
+        return true;
     }
 }
