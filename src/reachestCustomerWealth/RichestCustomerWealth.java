84ee4a2ee36a50abe16a9f6ad0dd0ba4169ce898
07elbekdev <masrapovelbek24@gmail.com>
2023-01-14 18:28:18 +0600

    I committed something.

--- src/reachestCustomerWealth/RichestCustomerWealth.java
+++ src/reachestCustomerWealth/RichestCustomerWealth.java
@@ -8,10 +8,10 @@ public class RichestCustomerWealth {
 
     public static int maximumWealth(int[][] accounts) {
         int maxSum = Integer.MIN_VALUE;
-        for (int i = 0; i < accounts.length; i++) {
+        for (int[] account : accounts) {
             int sum = 0;
             for (int j = 0; j < accounts[0].length; j++) {
-                sum += accounts[i][j];
+                sum += account[j];
             }
             if (maxSum < sum) {
                 maxSum = sum;
