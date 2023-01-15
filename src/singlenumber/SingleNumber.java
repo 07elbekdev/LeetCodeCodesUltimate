18312fcfe3e0f74b4028b1ad11142df528a2f313
07elbekdev <masrapovelbek24@gmail.com>
2023-01-15 19:29:36 +0600

    I changed something

--- src/singlenumber/SingleNumber.java
+++ src/singlenumber/SingleNumber.java
@@ -1,7 +1,25 @@
 package singlenumber;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class SingleNumber {
     public static void main(String[] args) {
+        int[] numbers = {1, 2, 2, 3, 4, 3, 4};
+        System.out.println(singleNumber(numbers));
+    }
 
+    public static int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new LinkedHashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
     }
 }
