54ae5430911572b407d4a6fa1fb0c7b4c121e4c0
07elbekdev <masrapovelbek24@gmail.com>
2023-01-21 15:26:12 +0600

    I committed something.

--- src/validPerfectSquare/ValidPerfectSquare.java
+++ src/validPerfectSquare/ValidPerfectSquare.java
@@ -2,11 +2,15 @@ package validPerfectSquare;
 
 public class ValidPerfectSquare {
     public static void main(String[] args) {
-        int number = 5;
+        int number = 16;
         System.out.println(isPerfectSquare(number));
     }
 
     public static boolean isPerfectSquare(int num) {
-        return true;
+
+        if (num == Math.sqrt(num) * Math.sqrt(num)) {
+            return true;
+        }
+        return false;
     }
 }
