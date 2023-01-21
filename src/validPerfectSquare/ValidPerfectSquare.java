9494498ef37329b944c0d194c01e969f59deec91
07elbekdev <masrapovelbek24@gmail.com>
2023-01-21 14:18:01 +0600

    I committed something.

---
+++ src/validPerfectSquare/ValidPerfectSquare.java
@@ -0,0 +1,12 @@
+package validPerfectSquare;
+
+public class ValidPerfectSquare {
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println(isPerfectSquare(number));
+    }
+
+    public static boolean isPerfectSquare(int num) {
+        return true;
+    }
+}
